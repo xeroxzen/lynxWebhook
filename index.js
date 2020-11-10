3da8aa8057d65c47b71d94bbc83bf4d9e5652873
xeroxzen <andilembele020@gmail.com>
2020-11-10 04:22:22 +0200

    Added webhookDemo and customPayloadDemo

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
 const express = require('express');
 const app = express();
+const dfff = require('dialogflow-fulfillment');
 
 app.get('/', (req, res)=> {
-    res.send("We are live")
+    res.send("Yes the server is live dude, go to bed.")
+});
+
+app.post('/', express.json(), (req, res)=>{
+    const agent = new dfff.WebhookClient({
+        request : req,
+        response : res
+    });
+
+    function demo(agent) {
+        agent.add("Sending response from Webhook server as [Version 1.1.11.1]");
+    }
+
+    function customPayloadDemo(agent){
+        var payloadData = {
+            "richContent": [
+                [
+                    {
+                        "type": "info",
+                        "title": "Info item title",
+                        "subtitle": "Info item subtitle",
+                        "image": {
+                        "src": {
+                            "rawUrl": "https://example.com/images/logo.png"
+                        }
+                        },
+                        "actionLink": "https://example.com"
+                    }
+                ]
+            ]
+        }
+
+        agent.add( new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true}) )
+    }
+    // Confirming #capture-fullname.firstname #capture-fullname.lastname with phone number $phone-number wishes to travel from #capture-to.travel-from to #capture-date.travel-to on #capture-schedule.travel-date.original in the #confirm-booking.travel-time
+
+    function confirmBooking(agent){
+        var fullname = agent.context("capture-fullname");
+        var phone = agent.context("phone-number");
+        var travelFrom = agent.context("capture-to");
+        var travelTo = agent.context("capture-date");
+        var travelDate = agent.context("capture-schedule");
+        var travelTime = agent.context("confirm-booking");
+    }
+
+    var intentMap = new Map();
+
+    intentMap.set('webhookDemo', demo)
+    intentMap.set('customPayloadDemo', customPayloadDemo)
+
+    agent.handleRequest(intentMap);
 });
 
 app.listen(3333, ()=>console.log("Server is live at port 3333"));
