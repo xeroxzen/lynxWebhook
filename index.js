ac32ae867c52c9b4971901c01c9134b6271ac261
xeroxzen <andilembele020@gmail.com>
2020-11-15 09:20:54 +0200

    Let's see this hack

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ app.post('/dialogflow-fulfillment', express.json(), (req, res)=>{
 
         // Save human readable date
         const dateObject = new Date();
+        const actualTravelDate = travelDate.toLocalString();
 
         agent.add(`BOOKING CONFIRMATION \nFULL NAME: ${firstname} ${lastname} \nPHONE NUMBER: ${phone} \nTRIP: ${travelFrom} to ${travelTo} \nDATE: ${travelDate} \nTIME: ${travelTime} \n\nSafe Travels with City Link Luxury Coaches`);
 
