49a5ed6737cc12880ccc78c5b8b76dc5211b460a
xeroxzen <andilembele020@gmail.com>
2020-12-19 11:50:12 +0200

    momentjs #8 changed formatting

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.post("/dialogflow-fulfillment", express.json(), (req, res) => {
     const dateObject = new Date();
 
     //new Unix TimeStamp
-    momentTravelDate = moment(travelDate, "YYYY-MM-DD HH:mm:ss").format("LLLL");
+    momentTravelDate = moment(travelDate, "YYYY-MM-DD HH:mm:ss").format("LL");
 
     // moment().format('LLLL');
 
